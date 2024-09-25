b18326191b6635a63c3a8970aafd9eb2e0398644
Halyna Rohatska <rohatskahr@gmail.com>
2024-09-25 16:02:00 +0300

    fixed

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -17,10 +17,10 @@ app_name = "todo"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
-    path("/create/", TaskCreateView.as_view(), name="task-create"),
-    path("/update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
-    path("/delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete"),
-    path("/<int:pk>/toggle/", toggle_task_status, name='toggle-task-status'),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete"),
+    path("<int:pk>/toggle/", toggle_task_status, name='toggle-task-status'),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagsCreateView.as_view(), name="tag-create"),
     path("tags/update/<int:pk>/", TagsUpdateView.as_view(), name="tag-update"),
